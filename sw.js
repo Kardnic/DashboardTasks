10db90cdebe88b214f417d4a113efebd4f4c16f0
Kardnic <158296649+Kardnic@users.noreply.github.com>
2026-09-23 08:48:44 +0200

    Sicheren App Cache neu versionieren

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dashboardtasks-v2';
+const CACHE='dashboardtasks-v4-secure';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
